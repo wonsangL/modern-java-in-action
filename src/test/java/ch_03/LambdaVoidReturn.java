dbc22965ea07f9350c99dbb62c9c3d40b32960c1
wonsangL <harryandws@gmail.com>
2022-02-12 16:19:19 +0900

    chapter 3.5.2 lambda void 호환 규칙 테스트 파라미터 타입 삭제

--- src/test/java/ch_03/LambdaVoidReturn.java
+++ src/test/java/ch_03/LambdaVoidReturn.java
@@ -7,12 +7,12 @@ import java.util.function.Consumer;
 public class LambdaVoidReturn {
     @Test
     public void lambdaVoidTest() {
-        Consumer<Integer> intConsumer = (Integer a) -> a.intValue();
+        Consumer<Integer> intConsumer = (a) -> a.intValue();
 
-//        Consumer<Integer> integerConsumer = (Integer a) -> a; compile error
+//        Consumer<Integer> integerConsumer = (a) -> a; compile error
 
-        Consumer<Boolean> booleanConsumer = (Boolean b) -> b.booleanValue();
+        Consumer<Boolean> booleanConsumer = (b) -> b.booleanValue();
 
-//        Consumer<Boolean> booleanClassConsumer = (Boolean b) -> b; compile error
+//        Consumer<Boolean> booleanClassConsumer = (b) -> b; compile error
     }
 }
