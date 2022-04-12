a44dcc9ad4ea4f7c62ac53e4861c3788c328dc07
wonsangL <harryandws@gmail.com>
2022-04-12 17:41:51 +0900

    Lambda memory access 예제 코드 작성

---
+++ src/test/java/ch_03/MemoryAccessTest.java
@@ -0,0 +1,40 @@
+package ch_03;
+
+import common.Apple;
+import common.Color;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MemoryAccessTest {
+    @Test
+    public void multi_thread_access_test() {
+        Thread thread = new Thread(() -> {
+            System.out.println("outer thread start!!");
+            Apple apple = new Apple();
+            apple.setColor(Color.RED);
+            apple.setWeight(10);
+
+            Thread internalThread = new Thread(() -> {
+                try {
+                    System.out.println("inner thread start!!");
+                    Thread.sleep(3000);
+                    Assert.assertEquals(Color.RED, apple.getColor());
+                    System.out.println("inner thread end!!");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            internalThread.start();
+            System.out.println("outer thread end!!");
+        });
+
+        thread.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
