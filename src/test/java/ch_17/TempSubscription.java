62f70d1cf8cbf96469a8503264a857fb6041e4fe
wonsangl <wonsangl@netmarble.com>
2022-06-22 20:42:55 +0900

    Chapter17. Reactor Executor 예제 코드

--- src/test/java/ch_17/TempSubscription.java
+++ src/test/java/ch_17/TempSubscription.java
@@ -3,9 +3,14 @@ package ch_17;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class TempSubscription implements Subscription {
     private Subscriber<? super TempInfo> subscriber;
     private final String town;
+
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public TempSubscription(Subscriber<? super TempInfo> subscriber, String town) {
         this.subscriber = subscriber;
@@ -14,14 +19,16 @@ public class TempSubscription implements Subscription {
 
     @Override
     public void request(long n) {
-        for (long i = 0L; i < n; i++) {
-            try {
-                subscriber.onNext(TempInfo.fetch(town));
-            } catch (Exception e) {
-                subscriber.onError(e);
-                break;
+        executor.submit(() -> {
+            for (int i = 0; i < n; ++i) {
+                try {
+                    subscriber.onNext(TempInfo.fetch(town));
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                    break;
+                }
             }
-        }
+        });
     }
 
     @Override
